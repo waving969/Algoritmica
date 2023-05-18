61221b8e14e48b00b418bf8be61821b447a0136e
waving969 <73184853+waving969@users.noreply.github.com>
2023-05-18 17:38:27 +0200

    heuristica

--- Practica3/Entrega/Codigo/Practica3.cpp
+++ Practica3/Entrega/Codigo/Practica3.cpp
@@ -6,30 +6,38 @@
 #include <algorithm>
 using namespace std;
 
-string menu(){
+string menu(int &nodoInicial){
     string devolver = "F";
-    int menu;
-    cout<< "Implementacion Practica3 \nSeleccion del grafo:\n \t1.- Grafo prueba\n\t2.- Grafo 1\n\t3.- Grafo 2\n\t4.- Grafo no euleriano\n\t5.- Grafo 3\n" << endl;
-    cin >> menu;
-
-    switch (menu)
-    {
-    case 1:
-        devolver = "./datos.txt"; 
-        break;
-    case 2:
-        devolver = "./Grafo1.txt"; 
-        break;
-    case 3:
-        devolver = "./Grafo2.txt"; 
-        break;
-    case 4: 
-        devolver = "./GrafoNoEuleriano.txt"; 
-        break;
-    case 5: 
-        devolver = "./grafo3.txt"; 
-        break;
     
+    int menu = 6;
+    while(menu == 6){    
+        
+        cout<< "Implementacion Practica3 \nSeleccion del grafo:\n \t1.- Grafo prueba\n\t2.- Grafo 1\n\t3.- Grafo 2\n\t4.- Grafo no euleriano\n\t5.- Grafo 3\n\t6.- Configuracion" << endl;
+        cin >> menu;
+
+        switch (menu)
+        {
+        case 1:
+            devolver = "./datos.txt"; 
+            break;
+        case 2:
+            devolver = "./Grafo1.txt"; 
+            break;
+        case 3:
+            devolver = "./Grafo2.txt"; 
+            break;
+        case 4: 
+            devolver = "./GrafoNoEuleriano.txt"; 
+            break;
+        case 5: 
+            devolver = "./grafo3.txt"; 
+            break;
+        case 6: 
+            cout << "Introduce el nodoInicial: ";
+            cin >> nodoInicial;
+            break;
+        
+        }
     }
     return devolver;
 }
@@ -65,30 +73,33 @@ bool esEuleriano(vector<vector<int>>& grafo) {
     return true;
 }
 
-int heuristicaGreedy(vector<vector<int>> &grafo, int nodo){
-    //cout << "tamanio nodo " << nodo <<  " aristas: " << grafo[nodo].size();
-    int masPrometedor = 0;
-    int posmasPrometedor = 0;
-    for (int i = 0; i < grafo[nodo].size(); i++){
-        if(grafo[].size() >= masPrometedor){
-            masPrometedor = grafo[i].size();
-            posmasPrometedor = i;
+int heuristicaGreedy(vector<vector<int>>& grafo, int v) {
+    // Elige el vecino con el menor número de aristas adyacentes
+    int minVecino = grafo[v][0];
+    int minAristas = grafo[grafo[v][0]].size();
+
+    for (int i = 1; i < grafo[v].size(); i++) {
+        int vecino = grafo[v][i];
+        int numAristas = grafo[vecino].size();
+
+        if (numAristas < minAristas) {
+            minVecino = vecino;
+            minAristas = numAristas;
         }
     }
 
-    return posmasPrometedor;
+    return minVecino;
 }
 
-// Función que realiza el recorrido de Euler
+
 void fleury(vector<vector<int>>& grafo, vector<int>& camino, int v) { 
     while (!grafo[v].empty()) {
         int w = heuristicaGreedy(grafo, v);
-        //int w = grafo[v][0];  // Selecciona el primer vecino de v
-        cout << "Borrando arista prometedora " << w;
-        grafo[v].erase(grafo[v].begin() + w);  // Elimina la arista (v,w) del grafo
-        for (int i = 0; i < grafo[w].size(); i++) {
+        //cout << "Borrando arista prometedora " << w;
+        grafo[v].erase(grafo[v].begin());  // Elimina la arista (v,w) del grafo
+        for (size_t i = 0; i < grafo[w].size(); i++) {
             if (grafo[w][i] == v) {  // Elimina la arista (w,v) del grafo
-            cout << " Borrando " << i << " De nodo " << w <<  endl;
+                //cout << " Borrando " << i << " De nodo " << w <<  endl;
                 grafo[w].erase(grafo[w].begin() + i);
                 break;
             }
@@ -98,14 +109,15 @@ void fleury(vector<vector<int>>& grafo, vector<int>& camino, int v) {
     camino.push_back(v);  // Agrega v al circuito de Euler
 }
 
-int main() {
-    vector<vector<int>> grafo = LeerFichero(menu());
 
-    int prueba = heuristicaGreedy(grafo, 0);
+int main() {
+    //string menue = menu();
+    int nodoInicial = 0;
+    vector<vector<int>> grafo =LeerFichero(menu(nodoInicial)); // {{1,2},{0,2,3,4},{0,1,3,4},{1,2,4,5},{1,2,3,5},{3,4}};
 
     if(esEuleriano(grafo)){
         vector<int> camino;
-        fleury(grafo, camino, 0);
+        fleury(grafo, camino, nodoInicial);
         cout << "El camino es: " << endl;
         for (int v : camino) {
             cout << v << " ";
