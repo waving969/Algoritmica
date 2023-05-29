7b53bfaa14c8f199afb32bc75f6a9553e9557653
waving969 <73184853+waving969@users.noreply.github.com>
2023-05-29 17:11:11 +0200

    final

--- Practica4/pruebas.cpp
+++ Practica4/pruebas.cpp
@@ -92,7 +92,7 @@ public:
             numAcciones--;
         }
 
-        cout << "Detalles compra: " << numAcciones << endl;
+        //cout << "Detalles compra: " << numAcciones << endl;
         return numAcciones;
     }
 
@@ -154,9 +154,9 @@ vector<tuple<Empresa, int>> algoritmoBasico(vector<Empresa> empresas, double &di
 
     for(auto it = listaEmpresasOrdenada.begin(); it != listaEmpresasOrdenada.end(); it++){
         Empresa empresa = *it; 
-        cout << "\nEmpresa: " << empresa.getNombre() << endl;
+        cout << "\nNombre empresa: " << empresa.getNombre() << endl;
         int numAcciones = empresa.numeroMaximoAccionesPuedoComprar(dineroInicial);
-        cout << "DInero antes de la compra: " << dineroInicial << endl;
+        cout << "Dinero antes de la compra: " << dineroInicial << endl;
         empresa.compraAcciones(dineroInicial, numAcciones);
         cout << "Dinero despues de la compra " << dineroInicial << endl;
         cout << "Numero de acciones de la empresa: " << empresa.getAcciones() << endl;
@@ -206,17 +206,18 @@ vector<tuple<Empresa, int>> algoritmoBasico2(vector<Empresa> empresas, double &d
 
     for(auto it = listaEmpresasOrdenada.begin(); it != listaEmpresasOrdenada.end(); it++){
         Empresa empresa = *it; 
-        cout << "\nEmpresa: " << empresa.getNombre() << endl;
+        
         if(empresa.getaccionXbeneficioXcomision() > 0){
+            cout << "\nNombre empresa: " << empresa.getNombre() << endl;
             int numAcciones = empresa.numeroMaximoAccionesPuedoComprar(dineroInicial);
-            cout << "DInero antes de la compra: " << dineroInicial << endl;
+            cout << "Dinero antes de la compra: " << dineroInicial << endl;
             empresa.compraAcciones(dineroInicial, numAcciones);
             cout << "Dinero despues de la compra " << dineroInicial << endl;
-            cout << "Numero de acciones de la empresa: " << empresa.getAcciones() << endl;
+            cout << "Numero de acciones de la empresa restantes: " << empresa.getAcciones() << endl;
             devolver.push_back(make_tuple(empresa, numAcciones));
         }
         else{
-            cout << "\nDe esta empresa no se compra: " <<  empresa.getaccionXbeneficioXcomision() << endl;
+            //cout << "\nDe esta empresa no se compra: " <<  empresa.getaccionXbeneficioXcomision() << endl;
         }
     }
     return devolver;
@@ -230,77 +231,14 @@ vector<tuple<Empresa, int>> algoritmoBasico2(vector<Empresa> empresas, double &d
     de cambio de monedas, para que no tengamos nuestra mayoria de acciones en una sola empresa y la tengamos "repartida" y sacando tambien el maximo beneficio
 
     Pasos: 
-    1.- Calcular el posible maximo beneficio por Accion de cada empresa 
-    2.- Ordenar las empresas por el beneficio maximo 
-    3.- Comprar el maximo de acciones de cada empresa siguiendo el orden anterior 
-    4.- Si no se puede comprar una accion, comprobar las empresas restantes hasta quedarse sin dinero 
+    1.- Crear la matriz de los posibles beneficios
+    2.- Plantear el caso base, que es cuando no se compran acciones, por tanto el beneficio es 0
+    3.- Plantear el problema como una ecuacion de maximizacion recursiva que devuelve el camino que debemos tomar, 
+        en este caso, el numero de acciones que compramos de cada empresa, donde en cada iteracion, tomará el mejor valor
+        para la compra de una unica accion
+    4.- Verificar que se cumple el P.O.B y crear el camino, eliminando las acciones redundantes y quedandose con el valor 
+        de las acciones mas altas para comprar,
 */
-
-
-
-double CambiodeMonedas(std::vector<Empresa>& empresas, int capital) {
-    int numEmpresas = empresas.size();
-    std::vector<double> dp(capital+1, INT_MIN);
-
-    dp[0] = 0;
-
-    for (int i = 0; i < capital; i++) {
-        for (int j = 0; j < numEmpresas; j++) {
-            if (i >= empresas[j].getPrecio()) {
-                double beneficioInvertir = empresas[j].calcularBeneficioTotal(1) + dp[i - empresas[j].getPrecio()];
-                dp[i] = std::max(dp[i], beneficioInvertir);
-            }
-        }
-    }
-
-    return dp[capital];
-}
-
-
-double Mochila(std::vector<Empresa>& empresas, int capital) {
-    int numEmpresas = empresas.size();
-
-    // Creamos una matriz para almacenar los beneficios máximos acumulados
-    std::vector<std::vector<double>> beneficios(numEmpresas + 1, std::vector<double>(capital + 1, 0.0));
-
-    for (int i = 1; i <= numEmpresas; i++) {
-        for (double j = 1; j <= capital; j++) {
-            if (empresas[i - 1].getPrecio() <= j) {
-                double beneficioInvertir = empresas[i - 1].calcularBeneficioTotal(1) + beneficios[i - 1][j - empresas[i - 1].getPrecio()];
-                beneficios[i][j] = std::max(beneficios[i - 1][j], beneficioInvertir);
-            }
-            else {
-                beneficios[i][j] = beneficios[i - 1][j];
-            }
-        }
-    }
-
-    return beneficios[numEmpresas][capital];
-}
-
-
-double ideaOriginal(std::vector<Empresa>& empresas, int capital) {
-    int numEmpresas = empresas.size();
-
-    // Creamos una matriz para almacenar los beneficios acumulados
-    std::vector<std::vector<double>> beneficios(numEmpresas + 1, std::vector<double>(capital + 1, 0.0));
-
-    for (double i = 1; i <= numEmpresas; i++) {
-        for (double j = 1; j <= capital; j++) {
-            double maxBeneficio = beneficios[i - 1][j];  // Beneficio máximo sin invertir en la empresa actual
-            
-            // Comprobamos si es posible invertir en la empresa actual con el capital disponible
-            if (j >= empresas[i - 1].getPrecio()) {
-                double beneficioInvertir = empresas[i - 1].calcularBeneficioTotal(j / empresas[i - 1].getPrecio()) + beneficios[i - 1][static_cast<int>(j) % static_cast<int>(empresas[i - 1].getPrecio())];
-                maxBeneficio = std::max(maxBeneficio, beneficioInvertir);
-            }
-            
-            beneficios[i][j] = maxBeneficio;
-        }
-    }
-
-    return beneficios[numEmpresas][capital];
-}
 
 bool findOnTuple(vector<tuple<Empresa, int>>lista, int i, int j, vector<Empresa> empresas){
     bool devolver = false; 
@@ -309,11 +247,11 @@ bool findOnTuple(vector<tuple<Empresa, int>>lista, int i, int j, vector<Empresa>
     for(const auto it: lista){
         Empresa elemento1 = get<0>(it);
         int elemento2 = get<1>(it);
-        cout << "Comparando "<< elemento1.getNombre() << " con " << empresas[i].getNombre()<< endl;
-        cout << "Numero de acciones " << elemento2 << " con " << j << endl;
+        //cout << "Comparando "<< elemento1.getNombre() << " con " << empresas[i].getNombre()<< endl;
+        //cout << "Numero de acciones " << elemento2 << " con " << j << endl;
 
         if(empresas[i].getNombre() == elemento1.getNombre() and elemento2 == j){
-            cout << "\tEncontrado similar " << endl;
+            //cout << "\tEncontrado similar " << endl;
             return true;
         }
             
@@ -330,6 +268,8 @@ vector<tuple<Empresa, int>> ideaOriginal2(std::vector<Empresa>& empresas, double
     vector<tuple<Empresa, int>> devolver;
     vector<Empresa> empre;
     int max = 0;
+
+
     //Caso base 
     for (int i = 0; i < numEmpresas; i++){
         beneficios[i][0] = 0;
@@ -341,9 +281,10 @@ vector<tuple<Empresa, int>> ideaOriginal2(std::vector<Empresa>& empresas, double
         }
     }
 
+    //Paso 1 Crear la matriz de beneficios
     cout << "Matriz beneficios " << endl; 
     for(int i = 0; i < numEmpresas; i++){
-        cout << empresas[i].getNombre() << " ";
+        cout << empresas[i].getNombre() << " \t";
         for(int j = 1; j <= NUM_ACCIONES; j++){
             cout << beneficios[i][j] << " ";
         }
@@ -352,14 +293,16 @@ vector<tuple<Empresa, int>> ideaOriginal2(std::vector<Empresa>& empresas, double
 
     
     bool puedeComprar = true; 
-    int capital_aux = capital;
+    double capital_aux = capital;
     int aux_i;
     int aux_j;
     vector<tuple<Empresa, int>> listaCompra;
     int contador = 0;
+    
 
+    //Paso 2
     while(capital_aux > 0 and puedeComprar){
-        cout << "\nIteracion " << ++contador << endl;
+        //cout << "\nIteracion " << ++contador << endl;
         max = 0;
         for(int j = 1; j <= NUM_ACCIONES; j++){
             for(int i = 0; i < numEmpresas; i++){
@@ -372,14 +315,15 @@ vector<tuple<Empresa, int>> ideaOriginal2(std::vector<Empresa>& empresas, double
             }
             if(max != 0){
                 capital_aux -= empresas[aux_i].getPrecio()+empresas[aux_i].getComision();
+                //cout << "Capitalaux: " << capital_aux << endl;
                 if(capital_aux < 0){
-                    cout << "Se acabo el dinero"<< endl;
+                    //cout << "Se acabo el dinero"<< endl;
                     puedeComprar = false;
-                    break;
+                    
                 }
                 //cout << "Compramos acciones de: " << empresas[aux_i].getNombre() << " Cantidad: " << aux_j << endl;
-                
-                    listaCompra.push_back(make_tuple(empresas[aux_i], aux_j));
+                else
+                listaCompra.push_back(make_tuple(empresas[aux_i], aux_j));
                 
             
             }
@@ -387,23 +331,52 @@ vector<tuple<Empresa, int>> ideaOriginal2(std::vector<Empresa>& empresas, double
         }
     }
 
-    //Hacer lista para recorrer el vector de empresas y modificarlo 
-    cout << "\nLista de la compra: " << endl;
+    Empresa finCompra;
+    listaCompra.push_back(make_tuple(finCompra, -1));
+
+    //Paso 3
+    //cout << "\nLista de la compra: " << endl;
+    string nombreAux = " ";
+    Empresa empresaAux; 
+    int numAccionesAux = 0;
+    int primeraIteracion = 0;
+    
     for(const auto &it: listaCompra){
+        //cout << "Devolver.size() = " << devolver.size() << endl;
         Empresa elemento1 = get<0>(it);
         int elemento2 = get<1>(it);
-        cout << "\nNombre empresa: " << elemento1.getNombre() << endl;
-        cout << "DInero antes de la compra: " << capital << endl;
-        elemento1.compraAcciones(capital, 1);
-        cout << "Dinero despues de la compra " << capital << endl;
-        cout << "Numero de acciones de la empresa: " << elemento1.getAcciones() << endl;
+        //cout << "\nNombre empresa: " << elemento1.getNombre() << " == " << empresaAux.getNombre() << endl;
+        
+        if(empresaAux.getNombre() == elemento1.getNombre()){
+            //cout << "if " << elemento2 << " > " << numAccionesAux << endl; 
+            if(numAccionesAux<elemento2){
+                numAccionesAux = elemento2;
+            }
+        }else{
+            if(primeraIteracion != 0)
+                devolver.push_back(make_tuple(empresaAux, numAccionesAux));
+
+            empresaAux = elemento1;
+            numAccionesAux = elemento2;
+            primeraIteracion++;
+        }
     }
+
+    cout << "\nVector devolver " << endl;
+    for(const auto &it: devolver){
+        Empresa elemento1 = get<0>(it);
+        int elemento2 = get<1>(it);
+        cout << "\nNombre empresa: " << elemento1.getNombre() << " Acciones compradas: " << elemento2 <<  endl;
+        cout << "Dinero antes de la compra: " << capital << endl;
+        elemento1.compraAcciones(capital, elemento2);
+        cout << "Dinero despues de la compra " << capital << endl;
+        cout << "Numero de acciones de la empresa restantes: " << elemento1.getAcciones() << endl;
             
-    
+    }
     
     
 
-    return listaCompra;
+    return devolver;
 }
 
 
@@ -422,12 +395,14 @@ int main(){
     Empresa empresa3("Chuches Paqui", NUM_ACCIONES,10,120.0,5.0);
     Empresa empresa4("Car-Wash Asuncion", NUM_ACCIONES,5,102.0,10.0);
     Empresa empresa5("Ferreteria Manolo",NUM_ACCIONES,5,200.0,1.0);
+    Empresa empresa6("Academia Los Patos", NUM_ACCIONES, 1, 300, 10);
     vector<Empresa> vectorEmpresas;
     vectorEmpresas.push_back(empresa1);
     vectorEmpresas.push_back(empresa2);
     vectorEmpresas.push_back(empresa3);
     vectorEmpresas.push_back(empresa4);
     vectorEmpresas.push_back(empresa5);
+    vectorEmpresas.push_back(empresa6);
     
 
     /*for (Empresa empresa : vectorEmpresas) {
@@ -440,13 +415,13 @@ int main(){
         cout << endl;
     }*/
     
-   cout << "Algoritmo basico" << endl; 
+   cout << "\n\n\tALGORITMO BASICO RESULTADOS" << endl; 
 
     vector<tuple<Empresa, int>> solucion;
     solucion = algoritmoBasico(vectorEmpresas, dinero);
     double posibleBeneficio = 0;
     vector<tuple<Empresa, int>> solucionPD;
-  /*  cout << "\n\tNumero de acciones compradas: " << endl; 
+    cout << "\n\tNumero de acciones compradas: " << endl; 
     for (const auto& it: solucion){
         Empresa elemento1 = get<0>(it);
         int elemento2 = get<1>(it); 
@@ -454,18 +429,18 @@ int main(){
         if(elemento2 != 0){
             cout << "\nEmpresa: "<< elemento1.getNombre() << " \nAcciones Compradas: " << elemento2 << endl;
             posibleBeneficio += elemento2 * elemento1.getBeneficios() * elemento1.getPrecio();
-            cout << "\nEmpresa, beneficio por accion: "<<elemento1.getBeneficios() << " * " << elemento2 << endl;
+            cout << "Empresa, beneficio por accion: "<<elemento1.getBeneficios() << " * " << elemento2 << endl;
         }
     }
 
-    cout << "Dinero Final: "<< dinero << endl;
+    cout << "\n\nDinero Final: "<< dinero << endl;
     cout << "Posible Beneficio " << posibleBeneficio << endl; 
     cout << "Dinero total " << dinero + posibleBeneficio << endl;
-*/
 
 
 
 
+    posibleBeneficio = 0;
 
     cout << "\n\n\tALGORITMOS DE PROGRAMACION DINAMICA RESULTADOS  " << endl;
     double capital = 100;
@@ -476,11 +451,11 @@ int main(){
 
         if(elemento2 != 0){
             cout << "\nEmpresa: "<< elemento1.getNombre() << " \nAcciones Compradas: " << elemento2 << endl;
-            posibleBeneficio += elemento2 * elemento1.getBeneficios() * elemento1.getPrecio();
-            cout << "\nEmpresa, beneficio por accion: "<<elemento1.getBeneficios() << " * " << elemento2 << endl;
+            posibleBeneficio += elemento2* elemento1.getBeneficios() * elemento1.getPrecio();
+            cout << "Empresa, beneficio por accion: "<<elemento1.getBeneficios() << " * " << elemento2 << endl;
         }
     }
-
+    cout << "\n\nDinero final: " << capital << endl;
     cout << "Posible Beneficio " << posibleBeneficio << endl; 
     cout << "Dinero total " << capital + posibleBeneficio << endl;
 
